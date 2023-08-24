ef45722f8affd14352b93dedeed97966524e9849
A VIDHYA SAGAR <sagarvidhya082002@gmail.com>
2023-08-24 15:01:04 +0530

    second commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,14 +6,13 @@ import { Videos, Sidebar } from "./";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
-  const [videos,setVideos] = useState([]);
-  
+  const [videos, setVideos] = useState(null);
 
   useEffect(() => {
-   
+    setVideos(null);
+
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-    .then((data) => setVideos(data.items))
-      
+      .then((data) => setVideos(data.items))
     }, [selectedCategory]);
 
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,16 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+
 import { logo } from "../utils/constants";
-import { Search } from "@mui/icons-material";
+import { SearchBar } from "./";
 
-import SearchBar from "./SearchBar";
-const Navbar = () =>  (
-    <Stack 
-    direction="row" 
-    alignItems="center" 
-    p={2} 
-    sx={{position: 'sticky',background:'#000000',top:0,justifyContent: 'space-between'}}
-    >
-    <Link to="/" style={{ display:'flex', alignItems: 'center'}}>
-      <img src={logo} height={45}/>
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
     </Link>
-    <SearchBar/>
+    <SearchBar />
+  </Stack>
+);
 
-    </Stack>
-  )
-
-
-export default Navbar
+export default Navbar;
